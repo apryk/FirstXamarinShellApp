71fc9c5df1a0dd8b6b2143747b9e7c0f0be01b76
Aleksandr Prykhodko <aleksandr.prykhodko@akumyn.com.au>
2021-03-28 16:15:15 +1100

    showing selected and favourite coffee items

--- MyAwsomeShellApp/Views/CoffeeEquipmentPage.xaml.cs
+++ MyAwsomeShellApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Input;
+using MyAwsomeShellApp.Models;
 using MyAwsomeShellApp.ViewModels;
 using Xamarin.Forms;
 
@@ -11,6 +12,29 @@ namespace MyAwsomeShellApp.Views
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
+        }
+
+        async void ListView_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            var coffee = ((ListView)sender).SelectedItem as Coffee;
+            if (coffee == null)
+                return;
+            await DisplayAlert("Coffee Selected", coffee.Name, "Ok");
+        }
+
+        void ListView_ItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        async void MenuItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var coffee = ((MenuItem)sender).BindingContext as Coffee; // binding context allows to get access to coffee
+
+            if (coffee == null)
+                return;
+            await DisplayAlert("Coffee Favorited", coffee.Name, "OK");
+
         }
     }
 }
